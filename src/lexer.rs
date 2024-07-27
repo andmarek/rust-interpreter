0f9ef97ce2843e291ef048955fea7a7c00f24cfd
Andrew Marek <andwmarek@gmail.com>
2024-07-27 08:23:45 -0700

    pre refactor

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,10 @@
-use token;
+use crate::token::token::{Token, *};
 
 struct Lexer {
     input: String,
     position: usize,
     read_position: usize,
-    ch: Option<u8>,
+    ch: Option<char>,
 }
 impl Lexer {
     pub fn new(input: String) -> Lexer {
@@ -25,42 +25,56 @@ impl Lexer {
         if self.read_position >= self.input.len() {
             self.ch = None;
         } else {
-            self.ch = Some(self.input.as_bytes()[self.read_position]);
+            self.ch = Some(self.input.as_bytes()[self.read_position] as char);
         }
         self.position = self.read_position;
         self.read_position += 1;
     }
-
-    fn next_token(&mut self) -> token::Token {
-        let token = match self.ch {
-            token::IDENT => "/",
-            token::IDENT(String) =>,
-            token::INT(String) => "",
-            token::ILLEGAL(char) => "",
-            token::EOF => "",
-
-            token::// Operators
-            token::POUND_SIGN => "",
-            token::ASTERISK => "",
-            token::UNDERSCORE => "",
-            token::LEFT_ANGLE_BRAKCET => "",
-            token::RIGHT_ANGLE_BRACKET => "",
-            token::DASH => "",
-            token::BACKTICK => "",
-            token::LEFT_BRACKET => "",
-            token::RIGHT_BRACKET => "",
-            token::LEFT_PARENS => "",
-            token::RIGHT_PARENS => "",
-            token::EXCLAMATION_MARK => "",
-            token::PLUS_SIGN => "",
-            token::DOT => "",
-            token::PIPE => "",
-            token::BACKSLASH => "",
+    fn new_token(token_type: String, ch: char) -> Token {
+        return Token {
+            token_type,
+            literal: ch.to_string(),
         };
     }
+    fn read_identifier(&mut self) -> String {
+        let mut identifier = String::new();
+        while let Some(ch) = self.ch {
+            if ch.is_alphabetic() || ch == '_' {
+                identifier.push(ch);
+                self.read_char();
+            } else {
+                break;
+            }
+        }
+        identifier
+    }
 
-    fn new_token(token_type: token::Token, ch: u8) {
-
+    fn next_token(&mut self) -> Token {
+        let tok = match self.ch {
+            Some(ch) => match ch {
+                '#' => Lexer::new_token(POUND_SIGN.to_string(), ch),
+                '*' => Lexer::new_token(ASTERISK.to_string(), ch),
+                '_' => Lexer::new_token(UNDERSCORE.to_string(), ch),
+                '<' => Lexer::new_token(LEFT_ANGLE_BRACKET.to_string(), ch),
+                '>' => Lexer::new_token(RIGHT_ANGLE_BRACKET.to_string(), ch),
+                '-' => Lexer::new_token(DASH.to_string(), ch),
+                '`' => Lexer::new_token(BACKTICK.to_string(), ch),
+                '[' => Lexer::new_token(LEFT_BRACKET.to_string(), ch),
+                ']' => Lexer::new_token(RIGHT_BRACKET.to_string(), ch),
+                '(' => Lexer::new_token(LEFT_PARENS.to_string(), ch),
+                ')' => Lexer::new_token(RIGHT_PARENS.to_string(), ch),
+                '!' => Lexer::new_token(EXCLAMATION_MARK.to_string(), ch),
+                '+' => Lexer::new_token(PLUS_SIGN.to_string(), ch),
+                '.' => Lexer::new_token(DOT.to_string(), ch),
+                '|' => Lexer::new_token(PIPE.to_string(), ch),
+                '\\' => Lexer::new_token(BACKSLASH.to_string(), ch),
+                _ => Lexer::new_token("Not found".to_string(), ch),
+            },
+            None => Lexer::new_token(EOF.to_string(), '\0'),
+        };
+        self.read_char();
+        self.read_position += 1;
+        return tok;
     }
 }
 
@@ -75,11 +89,51 @@ mod tests {
         assert_eq!(result, 4);
     }
     */
+    /*
+        {token.LET, "let"},
+        {token.IDENT, "five"},
+        {token.ASSIGN, "="},
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "ten"},
+        {token.ASSIGN, "="},
+        {token.INT, "10"},
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "add"},
+        {token.ASSIGN, "="},
+        {token.FUNCTION, "fn"},
+        {token.LPAREN, "("},
+        {token.IDENT, "x"},
+        {token.COMMA, ","},
+        {token.IDENT, "y"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},
+        {token.IDENT, "x"},
+        {token.PLUS, "+"},
+        {token.IDENT, "y"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "result"},
+        {token.ASSIGN, "="},
+        {token.IDENT, "add"},
+        {token.LPAREN, "("},
+        {token.IDENT, "five"},
+        {token.COMMA, ","},
+        {token.IDENT, "ten"},
+        {token.RPAREN, ")"},
+        {token.SEMICOLON, ";"},
+        {token.EOF, ""},
+    */
     #[test]
     fn test_next_token() {
         let test_string = String::from("this is a test.");
         let mut my_lexer = Lexer::new(test_string);
+        assert_eq!(my_lexer.ch, Some('t'));
         my_lexer.read_char();
-        assert_eq!(my_lexer.ch, 't');
+        assert_eq!(my_lexer.ch, Some('h'));
     }
 }
